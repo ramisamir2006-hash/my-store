008cd87430f5d9c95fae55d219daebfc96d705c2
ramisamir2006-hash <ramisamir2006@gmail.com>
2026-02-11 13:53:22 +0200

    Update store_main.py
    Signed-off-by: ramisamir2006-hash <ramisamir2006@gmail.com>

--- store_main.py
+++ store_main.py
@@ -1,8 +1,8 @@
 import os
 import threading
 import logging
-import telebot
-from telebot import types
+import pyTelegramBotAPI
+from pyTelegramBotAPI import types
 from flask import Flask
 
 # --- 1. إعدادات الربط النهائية (تأكد من التوكن الصحيح) ---
@@ -58,3 +58,4 @@ if __name__ == '__main__':
         
     except Exception as e:
         logger.error(f"❌ Critical error: {e}")
+
